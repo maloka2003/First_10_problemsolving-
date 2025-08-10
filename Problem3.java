b1e3ba3dc62abcc7f6a735db428fbe9753d0b595
maloka2003 <127336186+maloka2003@users.noreply.github.com>
2025-08-10 08:41:51 +0300

    Add files via upload

---
+++ Problem3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Problem3 {
+    public static void main(String[] args) {
+       // Scanner S=new Scanner(System.in);
+//        int L_weight,b_weight, count=0;
+//        L_weight=S.nextInt();
+//        b_weight=S.nextInt();
+//        S.nextLine();
+//        while(L_weight<=b_weight)
+//        {
+//            L_weight*=3;
+//            b_weight*=2;
+//            count++;
+//        }
+//        System.out.print(count);
+//        }
+
+        for(int i=0;i<=3;i++){
+            System.out.print(i);
+        }
+
+
+    }
+    }
